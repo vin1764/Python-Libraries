3c089171e38979b5cb4c9f7a04a12d1e59d1443f
vin1764 <126308173+vin1764@users.noreply.github.com>
2024-01-14 01:05:40 +0530

    Add files via upload

---
+++ 4.whileLoops.py
@@ -0,0 +1,14 @@
+i=0
+while i<10:
+    print(i)
+    i+=1
+
+#Break in while loop
+
+x=0
+while True:
+    print(x)
+    if x==23:
+        break
+    x+=1
+
---
+++ 5.exploringNewFunctions.py
@@ -0,0 +1,9 @@
+#To know what all functions are available for a data type, we can use the dir function. For eg,
+#if we want to know what all functiona can we perform on a string, we can use:
+
+print(dir(str)) #this will print the names of all available functions for string
+
+print(help(str)) #this will print the names as well as the functions of all functions 
+                 #available for string
+
+print(help(str.lower))  #this will print the function of the lower function of strings
